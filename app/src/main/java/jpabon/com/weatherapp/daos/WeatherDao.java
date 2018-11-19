9400e9170fe250ee1da28ce8301f2c531258c3b1
Jorge Pabon <jmpabon@gmail.com>
2018-11-20 03:53:39 +0800

    Update WeatherDao.java
    load method updated to retrieve only 2 registers

--- app/src/main/java/jpabon/com/weatherapp/daos/WeatherDao.java
+++ app/src/main/java/jpabon/com/weatherapp/daos/WeatherDao.java
@@ -9,6 +9,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import jpabon.com.weatherapp.entities.CityWeather;
 
+import static androidx.room.OnConflictStrategy.IGNORE;
 import static androidx.room.OnConflictStrategy.REPLACE;
 
 @Dao
@@ -16,7 +17,7 @@ public interface WeatherDao {
     @Insert(onConflict = REPLACE)
     void save(CityWeather cityweather);
 
-    @Query("SELECT * FROM CityWeather WHERE id IN (:city_ids)")
+    @Query("SELECT * FROM CityWeather WHERE id IN (:city_ids) LIMIT 2")
     LiveData<List<CityWeather>> load(List<Integer> city_ids);
 
     @Query("SELECT * FROM CityWeather WHERE id = :id LIMIT 5")
