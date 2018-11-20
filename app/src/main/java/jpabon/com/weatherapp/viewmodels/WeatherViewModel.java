f2845c1099fbb5f3e8a9e1ff7b9c8f2f51528a6c
Jorge Pabon <jmpabon@gmail.com>
2018-11-20 10:28:34 +0800

    Update WeatherViewModel.java
    Historic data from weather field in view model changed to mutable so it is possible to force the update on the UI after a spinner selection.
    Call to the repository is now done in a handler

--- app/src/main/java/jpabon/com/weatherapp/viewmodels/WeatherViewModel.java
+++ app/src/main/java/jpabon/com/weatherapp/viewmodels/WeatherViewModel.java
@@ -1,47 +1,53 @@
 package jpabon.com.weatherapp.viewmodels;
 
 import android.content.Context;
+import android.os.Handler;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import jpabon.com.weatherapp.entities.CityWeather;
 import jpabon.com.weatherapp.repository.WeatherRepository;
 
 public class WeatherViewModel extends ViewModel {
+
+
     LiveData<List<CityWeather>> cityWeather;
-    LiveData<List<CityWeather>> cityHistoric;
+    MutableLiveData<List<CityWeather>> cityHistoric;
 
     WeatherRepository weatherRepository;
 
-    public void init(Context context, List<Integer> cities) throws IllegalAccessException, UnsupportedEncodingException, InstantiationException {
+    public void init(Context context, List<Integer> cities, String units, String api_id) throws IllegalAccessException, UnsupportedEncodingException, InstantiationException {
         if (weatherRepository == null) {
             weatherRepository = new WeatherRepository(context);
         }
 
-        ReloadCityWeather(cities);
-        ReloadCityHistoric(cities.get(0));
+        ReloadCityWeather(cities, units, api_id);
+        cityHistoric = new MutableLiveData<>();
     }
 
-    public void ReloadCityWeather(List<Integer> cities) throws IllegalAccessException, UnsupportedEncodingException, InstantiationException {
-        cityWeather = weatherRepository.getWeatherForCities(cities);
+    public void ReloadCityWeather(List<Integer> cities, String units, String api_id) throws IllegalAccessException, UnsupportedEncodingException, InstantiationException {
+        cityWeather = weatherRepository.getWeatherForCities(cities, units, api_id);
     }
 
     public void ReloadCityHistoric(int id) {
-        cityHistoric = weatherRepository.getHistoricForCity(id);
+        Handler myHandler = new Handler();
+        myHandler.post(() -> {
+            List<CityWeather> info = weatherRepository.getHistoricForCity(id);
+
+            cityHistoric.setValue(info);
+        });
+
     }
 
     public LiveData<List<CityWeather>> getCityWeather() {
         return cityWeather;
     }
 
-    public LiveData<List<CityWeather>> getCityHistoric() {
+    public MutableLiveData<List<CityWeather>> getCityHistoric() {
         return cityHistoric;
-    }
-
-    public void setCityHistoric(LiveData<List<CityWeather>> cityHistoric) {
-        this.cityHistoric = cityHistoric;
     }
 }
